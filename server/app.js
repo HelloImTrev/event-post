ccdb9fa18d89eb423cade644141cf972483ecc4c
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-06-05 22:15:45 -0400

    google oauth progress

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const db = require('./db')
+const { User } = db.models
 const app = express();
+app.engine('html', require('ejs').renderFile);
 module.exports = app;
 const favicon = require("serve-favicon");
 require("dotenv").config();
@@ -17,7 +20,73 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/auth", require("./auth"));
 app.use("/api", require("./api"));
 
+//app.use(express.static(path.join(__dirname, '../public')));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "..", "public/index.html")));
+
+//*********************google oath**************************//
+
+//scopes
+const scopes = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'];
+
+const GOOGLE_AUTH_URI = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${process.env.GOOGLE_CALLBACK_URI}&prompt=consent&response_type=code&client_id=${process.env.GOOGLE_CLIENT_ID}&scope=${scopes.join(' ')}&access_type=offline`;
+
+// ????
+app.get('/googleOauth', (req, res)=> res.redirect(GOOGLE_AUTH_URI));
+
+app.get('/googleCallback', async (req, res, next)=> {
+  try {
+    res.send(`
+      <html>
+        <head>
+        </head>
+        <body>
+          <script>
+            window.localStorage.setItem('token', '${await User.loginViaGoogle(req.query.code)}');
+            window.location = '/';
+          </script>
+        </body>
+      </html>
+    `);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+//********************************************************************************************** */
+
+const renderHomePage = (req, res)=> {
+  res.render(path.join(__dirname, '..', 'public/index.html'));
+}
+
+app.get('/', renderHomePage);
+
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+  } else {
+    next()
+  }
+})
+
+// sends index.html
+app.use('*', renderHomePage); 
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+
+
 
 // static file-serving middleware
 app.use(express.static(path.join(__dirname, "..", "public")));
--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const axios = require("axios");
 
 const SALT_ROUNDS = 5;
 
@@ -56,6 +57,35 @@ User.findByToken = async function (token) {
     error.status = 401;
     throw error;
   }
+};
+
+// google oauth login class method
+User.loginViaGoogle = async function (code) {
+  const config = {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    grant_type: "authorization_code",
+    code,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    redirect_uri: process.env.GOOGLE_CALLBACK_URI,
+  };
+  let response = await axios.post(
+    "https://oauth2.googleapis.com/token",
+    config
+  );
+  response = await axios.get(
+    `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${response.data.access_token}`
+  );
+  const email = response.data.email;
+
+  let user = await this.findOne({
+    where: {
+      username: email,
+    },
+  });
+  if (!user) {
+    user = await this.create({ username: email });
+  }
+  return user.generateToken();
 };
 
 /**
