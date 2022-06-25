67822ea0dd7c2b5e38afa36b204ea61812f88e64
janie-lee-developer <janie.lee.inquire@gmail.com>
2022-06-25 10:17:42 -0400

    minor fix on search

--- client/components/helperComponents/SearchEngine.js
+++ client/components/helperComponents/SearchEngine.js
@@ -38,19 +38,19 @@ const SearchEngine = ({ explore, match }) => {
   useEffect(() => {
     getLocation();
     window.scrollTo(0, 0);
-    return () => {
-      setLocation({});
-      setError(null);
-    };
+    // return () => {
+    //   setLocation({});
+    //   setError(null);
+    // };
   }, []);
 
   useEffect(() => {
     if (location.state && !searchHistory.location)
       setSearchObj({ ...searchObj, location: location.state });
     dispatch(loadUserLocation(location));
-    return () => {
-      setSearchObj({});
-    };
+    // return () => {
+    //   setSearchObj({});
+    // };
   }, [location]);
 
   const getLocation = () => {
